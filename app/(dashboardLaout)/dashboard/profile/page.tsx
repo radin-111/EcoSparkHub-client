b1f767209fe62830e8a881a8432dcf548284fd4d
radin-111 <radinsheikh87@gmail.com>
2026-04-03 14:04:16 +0600

    fixing dynamic issue

--- app/(dashboardLaout)/dashboard/profile/page.tsx
+++ app/(dashboardLaout)/dashboard/profile/page.tsx
@@ -2,7 +2,7 @@ import { getSession } from "@/Actions/auth.action";
 import Profile from "@/components/shared/Profile";
 import { ApiResponse } from "@/types&enums&interfaces/api.types";
 import { SessionResponse } from "@/types&enums&interfaces/auth.types";
-import React from "react";
+export const dynamic = "force-dynamic";
 
 export default async function UserProfilePage() {
   const session = (await getSession()) as ApiResponse<SessionResponse>;
